00f929ee2a479138e0352390b632ab991047b07b
Muhammad Arsalan Nawazish <arsalan.nawazish@gmail.com>
2022-03-03 16:46:31 +0400

    Update

--- index.js
+++ index.js
@@ -21,14 +21,32 @@ mongoose.connect(database, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 
-console.log("Web app started")
+console.log('Chatroom System started at http://localhost:' + port);
 
 app.get('/', function(request, response) {
 	response.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/login', function(request, response) {
+	response.sendFile(path.join(__dirname + '/login.html'));
 });
 
 app.get('/register', function(request, response) {
 	response.sendFile(path.join(__dirname + '/register.html'));
 });
 
-app.listen(port);
+app.post('/auth', function(request, response) {
+
+});
+
+app.post('/registration', function(request, response) {
+
+});
+
+app.post('/account/verification/:code', function (request, response) {
+	let verCode = request.params.code
+});
+
+const listener = app.listen(port, function() {
+	console.log('Your app is listening on port ' + listener.address().port);
+}); 
