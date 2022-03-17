7d17481d9422cb2a794a2c420921f67333e65606
Muhammad Arsalan Nawazish <arsalan.nawazish@gmail.com>
2022-03-17 19:42:41 +0400

    Update

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(session({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'src')));
-//document.getElementById("name").innerHTML = request.session.nickname;
+
 //Database connection
 mongoose.connect(database, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = global.Promise;
