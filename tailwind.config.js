7d4666870fc0c87bea3ebabd653591955042e963
Muhammad Arsalan Nawazish <arsalan.nawazish@gmail.com>
2022-06-03 23:11:33 +0400

    Update

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ['src/chat.html', 'src/dashboard.html', 'src/forgot_password.html', 'src/login.html', 'src/register.html', 'src/reset_password.html','src/update_password.html', 'src/static/expired_link.html', 'src/static/sucessful_verification.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
